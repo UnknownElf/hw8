7993e524f6ec7f2c9a91cf01afb08e8f6cab86e1
UnknownElf <d_214676@yahoo.com.tw>
2016-11-02 15:42:00 +0800

    Add files via upload

---
+++ hw8-1.ino
@@ -0,0 +1,19 @@
+int ledPWM = 11;
+int counter = 0;
+
+void setup() {
+  pinMode(ledPWM, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //counter from 0 to 255
+  //makes the led from dark to light
+  counter++;
+  if(counter >= 256){
+    counter = 0;
+  }
+  Serial.println(counter);
+  analogWrite(ledPWM,counter);
+  delay(10);
+}
